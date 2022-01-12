ded0699ffc570bc236844123cca534f10e85598a
Szabolcs Toth <tsz@binaryrev.eu>
2022-01-12 14:39:46 +0100

    ColorExtension.swift finished.

--- Sources/BetaUI/ColorExtension.swift
+++ Sources/BetaUI/ColorExtension.swift
@@ -9,27 +9,26 @@ import SwiftUI
 
 public extension Color {
     
-     //MARK: Font colors
+    //MARK: Font colors
     static let bUIHeaderTextColor = Color("kMainColor1", bundle: .module)
-    /*
-     static let bUIFootnoteTextColor = kMainColor2
-     static let bUIInputTextFieldColor = kMainColor1
-     static let bUISecureTextFieldColor = kMainColor1
-     
-     //MARK: Border colors
-     static let bUIBorderColor = kMainColor1
-     
-     //MARK: Button colors
-     static let bUIButtonBaseColor1 = kMainColor1
-     static let bUIButtonBaseColor2 = kMainColor2
-     static let bUIButtonBaseColor3 = kMainColor3
-     static let bUIButtonTextColor = kMainColor4
-     static let bUIInverseButtonTextColor = kMainColor1
-     */
+    static let bUIFootnoteTextColor = Color("kMainColor2", bundle: .module)
+    static let bUIInputTextFieldColor = Color("kMainColor1", bundle: .module)
+    static let bUISecureTextFieldColor = Color("kMainColor1", bundle: .module)
     
+    //MARK: Border colors
+    static let bUIBorderColor = Color("kMainColor1", bundle: .module)
+    
+    //MARK: Button colors
+    static let bUIButtonBaseColor1 = Color("kMainColor1", bundle: .module)
+    static let bUIButtonBaseColor2 = Color("kMainColor2", bundle: .module)
+    static let bUIButtonBaseColor3 = Color("kMainColor3", bundle: .module)
+    static let bUIButtonTextColor = Color("kMainColor4", bundle: .module)
+    static let bUIInverseButtonTextColor = Color("kMainColor1", bundle: .module)
+     
     // Converting hex string to Color
     // https://stackoverflow.com/questions/24263007/how-to-use-hex-color-values/24263296
     // by Thomas Paul (https://stackoverflow.com/users/5235481/thomas-paul)
+    
     /// Usage: ```Color(hexString: "your_hex_string")```
     init(hexString: String) {
         let hex = hexString.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
