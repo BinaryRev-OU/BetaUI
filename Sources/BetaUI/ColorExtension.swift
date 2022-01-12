8fa4e6e8292919974c127bded8e449e205f09101
Szabolcs Toth <tsz@binaryrev.eu>
2022-01-12 13:13:12 +0100

    Updated ColorExtension.swift with `public`.

--- Sources/BetaUI/ColorExtension.swift
+++ Sources/BetaUI/ColorExtension.swift
@@ -7,25 +7,25 @@
 
 import SwiftUI
 
-extension Color {
+public extension Color {
     /*
-    //FIXME: Dark mode is not handled
-    //MARK: Font colors
-    static let bUIHeaderTextColor = kMainColor1
-    static let bUIFootnoteTextColor = kMainColor2
-    static let bUIInputTextFieldColor = kMainColor1
-    static let bUISecureTextFieldColor = kMainColor1
-    
-    //MARK: Border colors
-    static let bUIBorderColor = kMainColor1
-    
-    //MARK: Button colors
-    static let bUIButtonBaseColor1 = kMainColor1
-    static let bUIButtonBaseColor2 = kMainColor2
-    static let bUIButtonBaseColor3 = kMainColor3
-    static let bUIButtonTextColor = kMainColor4
-    static let bUIInverseButtonTextColor = kMainColor1
-    */
+     //FIXME: Dark mode is not handled
+     //MARK: Font colors
+     static let bUIHeaderTextColor = kMainColor1
+     static let bUIFootnoteTextColor = kMainColor2
+     static let bUIInputTextFieldColor = kMainColor1
+     static let bUISecureTextFieldColor = kMainColor1
+     
+     //MARK: Border colors
+     static let bUIBorderColor = kMainColor1
+     
+     //MARK: Button colors
+     static let bUIButtonBaseColor1 = kMainColor1
+     static let bUIButtonBaseColor2 = kMainColor2
+     static let bUIButtonBaseColor3 = kMainColor3
+     static let bUIButtonTextColor = kMainColor4
+     static let bUIInverseButtonTextColor = kMainColor1
+     */
     // Converting hex string to Color
     // Usage: Color(hexString: "your_hex_string")
     // https://stackoverflow.com/questions/24263007/how-to-use-hex-color-values/24263296
