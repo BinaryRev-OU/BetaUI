c012bbab8f31e803b23e00ccbb7a0954ac29f80a
Szabolcs Toth <tsz@binaryrev.eu>
2022-01-12 16:37:11 +0100

    InputStyles added.

---
+++ Sources/BetaUI/Components/InputStyles/BUIInputTextField.swift
@@ -0,0 +1,46 @@
+//
+//  InputTextField.swift
+//  BetaUI
+//
+//  Created by Szabolcs Tóth on 2021. 08. 10..
+//
+
+import SwiftUI
+
+public struct BUIInputTextField: View {
+    
+    @Binding var text: String
+    let placeholder: String
+    let keyboardType: UIKeyboardType
+    let sfSymbol: String?
+    let textLeading: CGFloat
+    
+    public init(text: Binding<String>, placeholder: String, keyboardType: UIKeyboardType, sfSymbol: String?, textLeading: CGFloat) {
+        self._text = text
+        self.placeholder = placeholder
+        self.keyboardType = keyboardType
+        self.sfSymbol = sfSymbol
+        self.textLeading = 30
+    }
+        
+    public var body: some View {
+        TextField(placeholder, text: $text)
+            .foregroundColor(.bUIInputTextFieldColor)
+            .frame(maxWidth: .infinity, minHeight: 44)
+            .padding(.leading, sfSymbol == nil ? textLeading / 2 : textLeading)
+            .autocapitalization(keyboardType == .default ? .words : .none)
+            .keyboardType(keyboardType)
+            .background(
+                ZStack(alignment: .leading) {
+                    if let systemImage = sfSymbol {
+                        Image(systemName: systemImage)
+                            .foregroundColor(checkIfTextFieldIsEmpty(text: text) ? Color.gray : .bUIInputTextFieldColor)
+                            .opacity(checkIfTextFieldIsEmpty(text: text) ? 0.3 : 1)
+                            .padding(.horizontal, 5)
+                    }
+                    
+                    BUIInputTextFieldBorder()
+                }
+            )
+    }
+}
---
+++ Sources/BetaUI/Components/InputStyles/BUISecureTextField.swift
@@ -0,0 +1,48 @@
+//
+//  BUISecureTextField.swift
+//  BetaUI
+//
+//  Created by Szabolcs Tóth on 2021. 08. 10..
+//
+
+import SwiftUI
+
+struct BUISecureTextField: View {
+    
+    
+    @Binding var password: String
+    let placeholder: String
+    let keyboardType: UIKeyboardType
+    let sfSymbol: String
+    let textLeading: CGFloat
+    
+    public init(password: Binding<String>, placeholder: String, keyboardType: UIKeyboardType, sfSymbol: String, textLeading: CGFloat) {
+        self._password = password
+        self.placeholder = placeholder
+        self.keyboardType = keyboardType
+        self.sfSymbol = sfSymbol
+        self.textLeading = 30
+    }
+    
+    var body: some View {
+        SecureField(placeholder, text: $password)
+            .foregroundColor(.bUISecureTextFieldColor)
+            .frame(maxWidth: .infinity, minHeight: 44)
+            .padding(.leading, textLeading)
+            .keyboardType(keyboardType)
+            .autocapitalization(.none)
+            .disableAutocorrection(true)
+            .background(
+                ZStack(alignment: .leading) {
+                    if let systemImage = sfSymbol {
+                        Image(systemName: systemImage)
+                            .foregroundColor(checkIfTextFieldIsEmpty(text: password) ? Color.gray : .bUIInputTextFieldColor)
+                            .opacity(checkIfTextFieldIsEmpty(text: password) ? 0.3 : 1)
+                            .padding(.horizontal, 5)
+                    }
+                    
+                    BUIInputTextFieldBorder()
+                }
+            )
+    }
+}
