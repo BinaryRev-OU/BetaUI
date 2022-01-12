27ce22ec523d2ecef005352a3e4feaac9d7c1a80
Szabolcs Toth <tsz@binaryrev.eu>
2022-01-12 13:18:17 +0100

    Color withHexString moved to function.

--- Sources/BetaUI/BetaUI.swift
+++ Sources/BetaUI/BetaUI.swift
@@ -1,6 +1,28 @@
+import SwiftUI
+
 public struct BetaUI {
     public private(set) var text = "Hello, World!"
-
+    
     public init() {
     }
+    
+    public func convColor(hexString: String) -> Color {
+        
+        let hex = hexString.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int = UInt64()
+        Scanner(string: hex).scanHexInt64(&int)
+        let r, g, b: UInt64
+        switch hex.count {
+        case 3:
+            (r, g, b) = ((int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6:
+            (r, g, b) = (int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8:
+            (r, g, b) = (int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (r, g, b) = (255, 0, 0)
+        }
+        return Color(red: Double(CGFloat(r)) / 255, green: Double(CGFloat(g)) / 255, blue: Double(CGFloat(b)) / 255)
+    }
+    
 }
