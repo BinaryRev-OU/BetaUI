fed2968fe91f0a5b78acf924b6ad35659df99056
Szabolcs Toth <tsz@binaryrev.eu>
2022-01-14 18:51:49 +0100

    Fixed Package.swift

--- Package.swift
+++ Package.swift
@@ -12,8 +12,11 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "BetaUI Components",
-            targets: ["BetaUI Components"]),
+            name: "BetaUI",
+            targets: ["BetaUI"]),
+        .library(
+            name: "BetaUI Images",
+            targets: ["BetaUI Images"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,12 +26,16 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "BetaUI Components",
+            name: "BetaUI",
             dependencies: [],
             path:"Sources/BetaUI/Components",
             resources: [
                 .process("Media.xcassets")
-            ]
+            ])
+        .target(
+            name: "BetaUI Images",
+            dependencies: ["BetaUI"],
+            path:"Sources/BetaUI/Images"
         )
     ]
 )
