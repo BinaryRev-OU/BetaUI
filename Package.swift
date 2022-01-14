8bc9b3258d741fb39744a51f79632bf1d68bfbab
Szabolcs Toth <tsz@binaryrev.eu>
2022-01-14 18:42:43 +0100

    Segregating components

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "BetaUI",
-            targets: ["BetaUI"]),
+            name: "BetaUI Components",
+            targets: ["BetaUI Components"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,15 +23,12 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "BetaUI",
+            name: "BetaUI Components",
             dependencies: [],
-            path:"Sources",
+            path:"Sources/BetaUI/Components",
             resources: [
                 .process("Media.xcassets")
             ]
-        ),
-        .testTarget(
-            name: "BetaUITests",
-            dependencies: ["BetaUI"]),
+        )
     ]
 )
