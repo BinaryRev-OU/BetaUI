aa9561dfca41c33bd8cc43bd50458ed2a482926b
Szabolcs Toth <tsz@binaryrev.eu>
2022-01-14 16:24:49 +0100

    Updating with drawing images instead of fixed color.

---
+++ Sources/BetaUI/Components/Images/BUIUserProfileImage/BUIUserProfileImageDrawView.swift
@@ -0,0 +1,16 @@
+//
+//  BUIUserProfileImageView.swift
+//  
+//
+//  Created by Szabolcs Tóth on 2022. 01. 14..
+//
+
+import UIKit
+
+public class BUIUserProfileImageDrawView: UIView {
+    
+    public override func draw(_ rect: CGRect) {
+        BUIImagesCode.drawBUIUserProfileImage()
+    }
+    
+}
